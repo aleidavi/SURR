aed1d02b3498dd8aab824def42ec40179fc84e5c
Aleida Vieyra <avieyra@ufl.edu>
2025-01-31 14:55:10 -0500

    debugging Heroku deployment

--- files/settings.py
+++ files/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://your-react-frontend-domain.com",
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this for collectstatic
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
