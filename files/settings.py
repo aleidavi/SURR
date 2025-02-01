59f8c6969b5c23a7dd0827e520b64d64dc5ee7f5
Aleida Vieyra <avieyra@ufl.edu>
2025-02-01 10:56:42 -0500

    uncommented lines 32-35 in settings.py for CORS_ALLOWES_ORIGINS

--- files/settings.py
+++ files/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#CORS_ALLOWED_ORIGINS = [
-    #"https://your-react-frontend-domain.com",
-    #"http://localhost:3000",  # for local development
-#]
+CORS_ALLOWED_ORIGINS = [
+    "https://your-react-frontend-domain.com",
+    "http://localhost:5173",  # for local development
+]
 
 ALLOWED_HOSTS = ['https://surr-92ba55f1fcdd.herokuapp.com/']
 
