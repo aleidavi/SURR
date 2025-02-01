cd6e892817fc306558ad7e10d87cd132e1606bb7
Aleida Vieyra <avieyra@ufl.edu>
2025-02-01 12:45:57 -0500

    deleted static files that were somehow saved to this branch

--- files/settings.py
+++ files/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False #os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
@@ -82,20 +82,20 @@ WSGI_APPLICATION = 'files.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # For Heroku Deployment Database configuration:
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
 #DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'surr_development',
-        #'USER': 'aleidavieyra',
-        #'PASSWORD': 'salmarios21',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
-    #}
+#    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'surr_development',
+        'USER': 'aleidavieyra',
+        'PASSWORD': 'salmarios21',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
