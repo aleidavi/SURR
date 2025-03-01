e37ba8fb443a3225f5e698d9a1c2ed55dd83d25a
aleidavi <avieyra@ufl.edu>
2025-03-01 16:34:48 -0500

    added serializer additions for Landlord and Property

--- files/serializers.py
+++ files/serializers.py
@@ -4,7 +4,7 @@ from .models import Landlord, Tenant, Property
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class LandlordSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Landlord
 		field = ['id', 'username', 'password']
@@ -17,11 +17,11 @@ class UserSerializer(serializers.ModelSerializer):
 			return landlord
 
 
-class LandlordSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Landlord
+# class LandlordSerializer(serializers.ModelSerializer):
+# 	class Meta:
+# 		model = Landlord
 
-		fields = '__all__'
+# 		fields = '__all__'
 
 
 class TenantSerializer(serializers.ModelSerializer):
@@ -34,6 +34,9 @@ class PropertySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Property
 		fields = '__all__'
+		extra_kwargs = {'landlord': {'read_only': True}}
+
+
 
 
 
