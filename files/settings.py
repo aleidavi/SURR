c712670dcf7abd07ab9a16b30099fc02d8819c95
aleidavi <avieyra@ufl.edu>
2025-03-31 16:47:08 -0400

    added a change to the settings.py file for DATABASES = {} configuration to connect to Heroku

--- files/settings.py
+++ files/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'files.wsgi.application'
 
 # For Heroku Deployment Database configuration:
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True, default=os.environ.get('DATABASE_URL'))
 }
 
 #DATABASES = {
