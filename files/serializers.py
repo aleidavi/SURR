f06e7cf4ceb14897677c378ccec47ed203e9be69
aleidavi <avieyra@ufl.edu>
2025-04-09 14:57:38 -0400

    moved class function to the correct indentation

--- files/serializers.py
+++ files/serializers.py
@@ -4,15 +4,18 @@ from django.contrib.auth.models import User
 
 
 
+
+
+
 class LandlordSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['id', 'username', 'password']
 		extra_kwargs = {'password': {'write_only': True}}
 
-		def create(self, validated_data):
-			landlord = User.objects.create_user(**validated_data)
-			return User
+	def create(self, validated_data):
+		landlord = User.objects.create_user(**validated_data)
+		return User
 
 
 class TenantSerializer(serializers.ModelSerializer):
