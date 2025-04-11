a772a16bd8bb6ffb81ee147c7bb9043ba03727b8
aleidavi <avieyra@ufl.edu>
2025-04-11 17:09:58 -0400

    removed previous views for landlord and property views

--- files/views/landlord_views.py
+++ files/views/landlord_views.py
@@ -23,52 +23,52 @@ class CreateLandlordView(generics.CreateAPIView):
 	permission_classes = [AllowAny]
 
 
-@api_view(['POST'])
-def landlord_list(request, format=None):
+# @api_view(['POST'])
+# def landlord_list(request, format=None):
 
-	#landlords_list = Landlord.objects.all()
-	#if request.method == 'GET':
-	#	serializer = LandlordSerializer(landlords_list, many=True)
-	#	return Response(serializer.data, status=status.HTTP_200_OK)
+# 	#landlords_list = Landlord.objects.all()
+# 	#if request.method == 'GET':
+# 	#	serializer = LandlordSerializer(landlords_list, many=True)
+# 	#	return Response(serializer.data, status=status.HTTP_200_OK)
 
-	if request.method == 'POST':
-		new_username = request.data.get('username', None)
-		if new_username == None:
-			return Response({'error': 'Username is required.'}, status=status.HTTP_400_BAD_REQUEST)
+# 	if request.method == 'POST':
+# 		new_username = request.data.get('username', None)
+# 		if new_username == None:
+# 			return Response({'error': 'Username is required.'}, status=status.HTTP_400_BAD_REQUEST)
 		
-		if Landlord.objects.filter(username=new_username).exists():
-			return Response({'error': 'The username entered is not available. Please choose a different username.'}, status=status.HTTP_400_BAD_REQUEST)
+# 		if Landlord.objects.filter(username=new_username).exists():
+# 			return Response({'error': 'The username entered is not available. Please choose a different username.'}, status=status.HTTP_400_BAD_REQUEST)
 
-		serializer = LandlordSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
+# 		serializer = LandlordSerializer(data=request.data)
+# 		if serializer.is_valid():
+# 			serializer.save()
 
-			# TO - DO -> Remove response message
-			# Used to return a response OBJ and a status value i.e. 201
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 			# TO - DO -> Remove response message
+# 			# Used to return a response OBJ and a status value i.e. 201
+# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 	
 
-@api_view(['GET','PATCH', 'DELETE'])
-def landlord_detail(request, landlord_id, format=None):
-		try:
-			landlord = Landlord.objects.get(pk=landlord_id)
-		except Landlord.DoesNotExist:
-			response_message = {'error message': f'This specified landlord {landlord_id}, does not exist.'}
-			return Response(response_message, status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET','PATCH', 'DELETE'])
+# def landlord_detail(request, landlord_id, format=None):
+# 		try:
+# 			landlord = Landlord.objects.get(pk=landlord_id)
+# 		except Landlord.DoesNotExist:
+# 			response_message = {'error message': f'This specified landlord {landlord_id}, does not exist.'}
+# 			return Response(response_message, status=status.HTTP_404_NOT_FOUND)
 	
-		if request.method == 'GET':
-			serializer = LandlordSerializer(landlord)
-			return Response(serializer.data, status=status.HTTP_200_OK)	
-		elif request.method == 'PATCH':
-			serializer = LandlordSerializer(landlord, data=request.data,partial=True)
-			if serializer.is_valid():
-				serializer.save()
+# 		if request.method == 'GET':
+# 			serializer = LandlordSerializer(landlord)
+# 			return Response(serializer.data, status=status.HTTP_200_OK)	
+# 		elif request.method == 'PATCH':
+# 			serializer = LandlordSerializer(landlord, data=request.data,partial=True)
+# 			if serializer.is_valid():
+# 				serializer.save()
 			
-			return Response(serializer.data, status=status.HTTP_200_OK)
+# 			return Response(serializer.data, status=status.HTTP_200_OK)
 		
-		elif request.method == 'DELETE':
-			landlord.delete()
-			response_message = {'message': f'Landlord {landlord_id}, data has been removed.'}
-			return Response(response_message, status=status.HTTP_200_OK)
+# 		elif request.method == 'DELETE':
+# 			landlord.delete()
+# 			response_message = {'message': f'Landlord {landlord_id}, data has been removed.'}
+# 			return Response(response_message, status=status.HTTP_200_OK)
--- files/views/property_views.py
+++ files/views/property_views.py
@@ -52,87 +52,87 @@ class PropertyDelete(generics.DestroyAPIView):
 
 
 
-@api_view(['GET', 'POST', 'DELETE']) 
-def landlord_property_list(request, landlord_id, format=None):
-	"""
-		Endpoint route allows landlord user to:
-			read all properties, of the current landlord,
-			post new properties (add/create) to their current properties list,
-			and delete a property from the properties list belonging to the landlord.
-	"""
-	try:
-		landlord = Landlord.objects.get(pk=landlord_id)
-	except Landlord.DoesNotExist:
-		response_message = {'message': f'Landlord {landlord_id} not found.'}
-		return Response(response_message, status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'POST', 'DELETE']) 
+# def landlord_property_list(request, landlord_id, format=None):
+# 	"""
+# 		Endpoint route allows landlord user to:
+# 			read all properties, of the current landlord,
+# 			post new properties (add/create) to their current properties list,
+# 			and delete a property from the properties list belonging to the landlord.
+# 	"""
+# 	try:
+# 		landlord = Landlord.objects.get(pk=landlord_id)
+# 	except Landlord.DoesNotExist:
+# 		response_message = {'message': f'Landlord {landlord_id} not found.'}
+# 		return Response(response_message, status=status.HTTP_404_NOT_FOUND)
 	
 
-	# GET All properties of landlord_id
-	landlord_property_list = landlord.properties.all()
+# 	# GET All properties of landlord_id
+# 	landlord_property_list = landlord.properties.all()
 
-	if request.method == 'GET':
-		serializer = PropertySerializer(landlord_property_list, many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+# 	if request.method == 'GET':
+# 		serializer = PropertySerializer(landlord_property_list, many=True)
+# 		return Response(serializer.data, status=status.HTTP_200_OK)
 	
-	# POST new property to landlord_id
-	elif request.method == 'POST':
+# 	# POST new property to landlord_id
+# 	elif request.method == 'POST':
 		
 		
-		updated_data = request.data
-		updated_data['landlord'] = landlord_id
-		serializer = PropertySerializer(data=updated_data)
-		if serializer.is_valid():
-			serializer.save()
+# 		updated_data = request.data
+# 		updated_data['landlord'] = landlord_id
+# 		serializer = PropertySerializer(data=updated_data)
+# 		if serializer.is_valid():
+# 			serializer.save()
 
-			# Used to return a response OBJ and a status value i.e. 201
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 			# Used to return a response OBJ and a status value i.e. 201
+# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	# DELETE a property from landlord_id properties
-	elif request.method == 'DELETE':
-		current_property_id = request.data.get('id', None)
+# 	# DELETE a property from landlord_id properties
+# 	elif request.method == 'DELETE':
+# 		current_property_id = request.data.get('id', None)
 		
-		if current_property_id is None:
-			return Response({'error': 'Property selected for deletion is invalid.'}, status=status.HTTP_400_BAD_REQUEST)
+# 		if current_property_id is None:
+# 			return Response({'error': 'Property selected for deletion is invalid.'}, status=status.HTTP_400_BAD_REQUEST)
 
 		
-		property_instance = get_object_or_404(Property, pk=current_property_id, landlord=landlord)
-		property_instance.delete()
+# 		property_instance = get_object_or_404(Property, pk=current_property_id, landlord=landlord)
+# 		property_instance.delete()
 		
-		response_message = {'message': f'Property {current_property_id}, data has been removed from Landlord {landlord_id}.'}
-		return Response(response_message, status=status.HTTP_200_OK)
+# 		response_message = {'message': f'Property {current_property_id}, data has been removed from Landlord {landlord_id}.'}
+# 		return Response(response_message, status=status.HTTP_200_OK)
 
 
-@api_view(['GET', 'PATCH'])
-def landlord_property_detail(request, landlord_id, property_id, format=None):
-	"""
-	 GET /landlords/landlord_id/properties/<property_id> -> 
-	 	Get the current property of property_id, for landlord_id specified
+# @api_view(['GET', 'PATCH'])
+# def landlord_property_detail(request, landlord_id, property_id, format=None):
+# 	"""
+# 	 GET /landlords/landlord_id/properties/<property_id> -> 
+# 	 	Get the current property of property_id, for landlord_id specified
 		 
-	 PATCH /landlords/landlord_id/properties/<property_id> ->
-	 	Update the current property of property_id for landlord_id specified
+# 	 PATCH /landlords/landlord_id/properties/<property_id> ->
+# 	 	Update the current property of property_id for landlord_id specified
 
-	"""
-
-
-	try:
-		current_property = Property.objects.get(pk=property_id)
-	except Property.DoesNotExist:
-		response_message = {'message': f'Property {property_id} not found.'}
-		return Response(response_message, status=status.HTTP_404_NOT_FOUND)
-
-	if request.method == 'GET':
-		serializer = PropertySerializer(current_property)
-		return Response(serializer.data, status=status.HTTP_200_OK)
+# 	"""
 
 
-	elif request.method == 'PATCH':
-		serializer = PropertySerializer(current_property, data=request.data, partial=True)
-		if serializer.is_valid():
-			serializer.save()
-		response_message={'message': f'Property {property_id} information successfully updated.'}
-		return Response(serializer.data, status=status.HTTP_200_OK)
+# 	try:
+# 		current_property = Property.objects.get(pk=property_id)
+# 	except Property.DoesNotExist:
+# 		response_message = {'message': f'Property {property_id} not found.'}
+# 		return Response(response_message, status=status.HTTP_404_NOT_FOUND)
+
+# 	if request.method == 'GET':
+# 		serializer = PropertySerializer(current_property)
+# 		return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# 	elif request.method == 'PATCH':
+# 		serializer = PropertySerializer(current_property, data=request.data, partial=True)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 		response_message={'message': f'Property {property_id} information successfully updated.'}
+# 		return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
